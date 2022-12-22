bea829015c0bed8670603fdeb9b86a68ed2c39a2
Seungyeon Baek <72596522+yeonannab@users.noreply.github.com>
2022-12-23 01:16:00 +0900

    [Fix] Fix Parameter matching error

--- Web/flask/sep_song.py
+++ Web/flask/sep_song.py
@@ -22,11 +22,12 @@ stroke_fill_2 = (69,91,125)
 
 def song_by_song(song_num,level='easy'):
     # 노래에 따른 변수 생성
-    correct_actions = globals()['correct_actions_' + song_num]
-    correct_act_ko = globals()['correct_act_ko_' + song_num]
-    cut_time_original = globals()['cut_time_' + song_num]
+    song_num_str=str(song_num)
+    correct_actions = globals()['correct_actions_' + song_num_str]
+    correct_act_ko = globals()['correct_act_ko_' + song_num_str]
+    cut_time_original = globals()['cut_time_' + song_num_str]
     plus_list = [6] * len(cut_time_original) # 카메라 delay 시간
     cut_time = [a+b for a,b in zip(cut_time_original,plus_list)] # action 실행 구간
-    stroke_fill = globals()['stroke_fill_' + song_num]
+    stroke_fill = globals()['stroke_fill_' + song_num_str]
 
     return correct_actions, correct_act_ko, cut_time, stroke_fill
